b1015645353a798c3b71d6f245c5f480a75dcd96
vaishnaviu <vaishnaviu96@gmail.com>
2023-10-09 17:00:24 -0700

    Uncommented the call for round robin function

--- algos.h
+++ algos.h
@@ -14,7 +14,7 @@ typedef struct average_stats averageStats;
 averageStats firstComeFirstServe(Queue *processes);
 // averageStats shortest_job_first_np(Queue *processes);
 // averageStats shortest_remaining_time_p(Queue *processes);
-// averageStats round_robin_p(Queue *processes,int time_slice);
+averageStats roundRobinPreemptive(Queue *processes,int time_slice);
 averageStats highest_priority_first_p(Queue *processes);
 // averageStats highest_priority_first_np(Queue *processes);
 // averageStats highest_priority_first_p_aging(Queue *processes);
